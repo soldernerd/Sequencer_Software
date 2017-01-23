2a126a2b910ed843517d2511e119367723ac6720
Lukas Fässler <lfaessler@gmx.net>
2017-01-24 00:43:00 +0100

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Sequencer.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Sequencer.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=sequencer.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sequencer.tar
---
+++ sequencer.c
@@ -0,0 +1,14 @@
+/*
+ * File:   sequencer.c
+ * Author: Luke
+ *
+ * Created on 24. Januar 2017, 00:41
+ */
+
+
+#include <xc.h>
+
+void main(void) 
+{
+    return;
+}
